8a19f1d4c79c3a4bebf0f8bdb226026d9a4e0b56
Gantulga <153663623+SoraxGanaa@users.noreply.github.com>
2025-11-30 08:31:30 +0800

    fix: dockers

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,6 +6,7 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
